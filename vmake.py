79afd49f9a8bf550e179ef6f269b357716a69ffa
Patrick Huesmann <info@patrick-huesmann.de>
2021-03-14 18:47:17 +0100

    Improve regex & add comments

--- vmake.py
+++ vmake.py
@@ -4,17 +4,19 @@ import subprocess
 import sys
 import re
 
+# Match file path and file location (row:column) from GCC error message
 re_gcc_file_loc = [
-    re.compile(r'^(.*):(\d+):(\d+): ' + msg + ':(.*)$')
+    re.compile(r'^(?P<f_path>.*):(?P<f_loc>\d+:?\d*): ' + msg + '.+$')
     for msg in (
         'error',
         'warning',
         'note',
-        'undefined reference'
+        'undefined reference',
     )
 ]
-# TODO: also handle messages w/o column value
 
+# Create shell hyperlink
+# https://gist.github.com/egmontkob/eb114294efbcd5adb1944c9f3cb5feda
 def shell_hyperlink(url, txt):
     result = '\x1b]8;;'
     result += url
@@ -23,8 +25,10 @@ def shell_hyperlink(url, txt):
     result += '\x1b]8;;\x1b\\'
     return result
 
-def vscode_link_msg(orig_msg, f_path, f_row, f_col):
-    f_desc = f'{f_path}:{f_row}:{f_col}'
+# Display hyperlink for VSCode URL handler
+# https://code.visualstudio.com/docs/editor/command-line#_opening-vs-code-with-urls
+def vscode_link_msg(orig_msg, m):
+    f_desc = m['f_path'] + ':' + m['f_loc']
     vsc_url = f'vscode://file/{f_desc}'
     n = len(f_desc)
     orig_msg = orig_msg[:n], orig_msg[n:]
@@ -37,8 +41,7 @@ for line in process.stderr:
     m = list(filter(None, [r.match(line) for r in re_gcc_file_loc]))
     if len(m):
         m = m[0]
-        orig_msg, f_path, f_row, f_col = [m.group(i) for i in (0, 1, 2, 3)]
-        vscode_link_msg(orig_msg, f_path, f_row, f_col)
+        vscode_link_msg(m[0], m.groupdict())
     else:
         sys.stderr.write(line)
 
