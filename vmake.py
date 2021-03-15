e311a4b37325151b41e814b94ca28a0ab912fa6d
Patrick Huesmann <info@patrick-huesmann.de>
2021-03-15 11:33:34 +0100

    Add workaround for Tmux (not reliable)

--- vmake.py
+++ vmake.py
@@ -2,7 +2,9 @@
 
 import subprocess
 import sys
+import os
 import re
+import time
 
 # Match file path and file location (row:column) from GCC error message
 re_gcc_file_loc = [
@@ -15,15 +17,23 @@ re_gcc_file_loc = [
     )
 ]
 
+# Pass through wrapper for tmux to send escape sequence to terminal emulator
+def tmux_wrap(msg):
+    if not tmux_mode:
+        return msg
+    sys.stdout.flush()
+    time.sleep(0.2)
+    return '\x1bPtmux;\x1b' + msg + '\x1b\\'
+
 # Create shell hyperlink
 # https://gist.github.com/egmontkob/eb114294efbcd5adb1944c9f3cb5feda
 def shell_hyperlink(url, txt):
-    result = '\x1b]8;;'
-    result += url
-    result += '\x1b\\'
-    result += txt
-    result += '\x1b]8;;\x1b\\'
-    return result
+    HL_OSC8 = '\x1b]8;;'
+    HL_ST = '\x07'
+
+    sys.stdout.write(tmux_wrap(HL_OSC8 + url + HL_ST))
+    sys.stdout.write(txt)
+    sys.stdout.write(tmux_wrap(HL_OSC8 + HL_ST))
 
 # Display hyperlink for VSCode URL handler
 # https://code.visualstudio.com/docs/editor/command-line#_opening-vs-code-with-urls
@@ -32,8 +42,10 @@ def vscode_link_msg(orig_msg, m):
     vsc_url = f'vscode://file/{f_desc}'
     n = len(f_desc)
     orig_msg = orig_msg[:n], orig_msg[n:]
-    l = shell_hyperlink(vsc_url, orig_msg[0])
-    sys.stderr.write(l + orig_msg[1] + '\n')
+    shell_hyperlink(vsc_url, orig_msg[0])
+    sys.stdout.write(orig_msg[1] + '\n')
+
+tmux_mode = os.environ.get('TMUX') is not None
 
 process = subprocess.Popen('make', stdout=sys.stdout, stderr=subprocess.PIPE)
 for line in process.stderr:
