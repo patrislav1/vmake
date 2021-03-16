b9e03daf78ecba6dc2b7a4866de4e3dae4ced4e1
Patrick Huesmann <info@patrick-huesmann.de>
2021-03-16 11:33:25 +0100

    Support gcc colored output
    Use pty to make gcc believe it's runnning in a tty

--- vmake/vmake.py
+++ vmake/vmake.py
@@ -2,11 +2,17 @@
 
 import subprocess
 import sys
+import os
+import select
+import pty
 import re
 
-# Match file path and file location (row:column) from GCC error message
+# GCC color formatting (non-matched group, optional)
+R_FMT = r'(?:\x1b\[\d*;?\d*m\x1b\[K)?'
+
+# Match file msg (text for hyperlink), file path, file location (row:column) and error msg. from GCC message
 re_gcc_file_loc = [
-    re.compile(r'^(?P<f_path>.*):(?P<f_loc>\d+:?\d*): ' + msg + '.+$')
+    re.compile(r'^(?P<f_msg>' + R_FMT + r'(?P<f_path>.*?):(?P<f_loc>\d+:?\d*):' + R_FMT + r')(?P<err_msg>\s?' + R_FMT + msg + r'.+$)')
     for msg in (
         'error',
         'warning',
@@ -27,25 +33,66 @@ def shell_hyperlink(url, txt):
 
 # Display hyperlink for VSCode URL handler
 # https://code.visualstudio.com/docs/editor/command-line#_opening-vs-code-with-urls
-def vscode_link_msg(orig_msg, m):
+def vscode_link_msg(m):
     f_desc = m['f_path'] + ':' + m['f_loc']
     vsc_url = f'vscode://file/{f_desc}'
-    n = len(f_desc)
-    orig_msg = orig_msg[:n], orig_msg[n:]
-    l = shell_hyperlink(vsc_url, orig_msg[0])
-    sys.stderr.write(l + orig_msg[1] + '\n')
+    l = shell_hyperlink(vsc_url, m['f_msg'])
+    return l + m['err_msg'] + '\n'
 
 def main():
     args = ['make'] + sys.argv[1:]
-    process = subprocess.Popen(args, stdout=sys.stdout, stderr=subprocess.PIPE)
-    for line in process.stderr:
-        line = line.decode('utf-8')
-        m = list(filter(None, [r.match(line) for r in re_gcc_file_loc]))
-        if len(m):
-            m = m[0]
-            vscode_link_msg(m[0], m.groupdict())
-        else:
-            sys.stderr.write(line)
+
+    # based on https://gist.github.com/hayd/4f46a68fc697ba8888a7b517a414583e
+    mo, so = pty.openpty() # provide tty to enable line-buffering
+    me, se = pty.openpty()
+    mi, si = pty.openpty()
+    fdmap = {
+        mo: {
+            'file': sys.stdout,
+            'buf': b'',
+        },
+        me:  {
+            'file': sys.stderr,
+            'buf': b'',
+        },
+        mi: None
+    }
+
+    process = subprocess.Popen(
+        args,
+        bufsize=1, stdin=si, stdout=so, stderr=se,
+        close_fds=True
+    )
+
+    timeout = .01
+    while True:
+        ready, _, _ = select.select([mo, me], [], [], timeout)
+        if ready:
+            for fd in ready:
+                data = os.read(fd, 512)
+                if not data:
+                    break
+
+                # Process line-wise
+                buf_str = fdmap[fd]['buf'] + data
+                while b'\n' in buf_str:
+                    k = buf_str.index(b'\n')+1
+                    line, buf_str = buf_str[:k], buf_str[k:]
+                    line = line.decode('utf-8')
+
+                    # Match regexes
+                    m = list(filter(None, [r.match(line) for r in re_gcc_file_loc]))
+                    if len(m):
+                        line = vscode_link_msg(m[0].groupdict())
+                    fdmap[fd]['file'].write(line)
+
+                fdmap[fd]['buf'] = buf_str
+
+        elif process.poll() is not None:  # select timed-out
+            break  # p exited
+
+    for fd in [si, so, se, mi, mo, me]:
+        os.close(fd)  # can't do it sooner: it leads to errno.EIO error
 
     process.wait()
     sys.exit(process.returncode)
